96e5d720e5565b9117d4cbdb489caf179ccc4184
Pierre Chouteau <pierrechouteau337@gmail.com>
2021-12-03 10:56:10 +0100

    Ajout de la fonction addition de matrice avec cuda

--- TP1.cu
+++ TP1.cu
@@ -43,6 +43,9 @@ void MatrixAdd(float *M1, float *M2, float *Mout, int n, int p){
 //Addition de deux matrices sur GPU
 __global__ void cudaMatrixAdd(float *M1, float *M2, float *Mout, int n, int p){
     
+    for(int i=0; i<n*p; i++){
+        Mout[i] = M1[i]+M2[i];
+    }
 }
 
 //Multiplication de deux matrices NxN sur CPU
@@ -62,21 +65,22 @@ int main(){
     printf("Hello from the CPU!\n\n");
     
     //Test de MatrixInit et MatrixPrint
-    printf("Here is our matrix!\n\n");
+    //printf("Here is our matrix!\n\n");
     
     float *M;    
     
-    int n = 3;
-    int p = 3;
+    int n = 5;
+    int p = 5;
     
     //Allocation de la mémoire pour la création de la matrice
     M = (float*)malloc(n * p * sizeof(float));
     
     MatrixInit(M, n, p);
-    MatrixPrint(M, n, p);
+    //MatrixPrint(M, n, p);
     
-    printf("\n");
+    //printf("\n");
     
+    free(M);
     
     //Test de MatrixAdd
     float *M1;
@@ -92,10 +96,43 @@ int main(){
     MatrixInit(M2, n, p);
     MatrixAdd(M1, M2, Mout, n, p);
     
+//     printf("Matrice 1\n");
+//     MatrixPrint(M1, n, p);
+//     printf("\nMatrice 2\n");
+//     MatrixPrint(M2, n, p);
+//     printf("\nMatrice résultante de la somme:\n");
+//     MatrixPrint(Mout, n, p);
+
+    //Test de cudaMatrixAdd
+    printf("Hello from the GPU!\n\n");
+    float *d_M1, *d_M2, *d_Mout;
+    
+    //Allocation des mémoires des matrices pour cuda
+    cudaMalloc((void**)&d_M1, sizeof(float)*n*p);
+    cudaMalloc((void**)&d_M2, sizeof(float)*n*p);
+    cudaMalloc((void**)&d_Mout, sizeof(float)*n*p);
+
+    cudaMemcpy(d_M1, M1, sizeof(float) * n * p, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_M2, M2, sizeof(float) * n * p, cudaMemcpyHostToDevice);
+
+    //Addition sur GPU
+    cudaMatrixAdd<<<1, n>>>(d_M1, d_M2, d_Mout, n, p);
+    
+    //Copie du résultat sur CPU
+    cudaMemcpy(Mout, d_Mout, sizeof(float)*n*p, cudaMemcpyDeviceToHost);
+    
     printf("Matrice 1\n");
     MatrixPrint(M1, n, p);
     printf("\nMatrice 2\n");
     MatrixPrint(M2, n, p);
     printf("\nMatrice résultante de la somme:\n");
     MatrixPrint(Mout, n, p);
+    
+    cudaFree(d_M1);
+    cudaFree(d_M2);
+    cudaFree(d_Mout);
+    
+    free(M1);
+    free(M2);
+    free(Mout);
 }
